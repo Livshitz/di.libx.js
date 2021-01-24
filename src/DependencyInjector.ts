cb741001a9616bbd6f0bd7250c8665bf3b52a960
Adam Bocco <adam.bocco@gmail.com>
2021-01-24 10:31:57 -0500

    Changed var to let or const

--- src/DependencyInjector.ts
+++ src/DependencyInjector.ts
@@ -30,8 +30,8 @@ export default class DependencyInjector {
     }
 
     public async registerResolve(name, func) {
-        var ret = new Deferred();
-        var realInstance = await this.require(func);
+        const ret = new Deferred();
+        const realInstance = await this.require(func);
         this.register(realInstance, name)
         ret.resolve(realInstance);
         return ret;
@@ -40,7 +40,7 @@ export default class DependencyInjector {
     public get<T>(name: string): T {
         if (name == null) name = Helpers.tryGetInstanceTypeName<T>();
 
-        var ret = this.modules[name];
+        let ret = this.modules[name];
 
         if (ret == null && this._resolver != null) { // try to require a module
             ret = this._resolver(name);
@@ -50,33 +50,33 @@ export default class DependencyInjector {
     }
 
     private resolve<T>(func: Function, isGetArray=false): T[] {
-        var modulesName = Helpers.getParamNames(func);
+        const modulesName = Helpers.getParamNames(func);
         if (modulesName == null) return null
         if (modulesName.length == 1 && !isGetArray) return this.get(modulesName);
 
-        var ret = [];
+        let ret = [];
         modulesName.forEach(m=> ret.push(this.get(m)));
         return ret;
     }
 
     public inject (func) {
-        var modules = this.resolve(func, true);
+        const modules = this.resolve(func, true);
         return func.apply(func, modules);
     }
 
     public async require<T>(func: Function): Promise<T> {
-        var modulesName = Helpers.getParamNames(func);
+        const modulesName = Helpers.getParamNames(func);
         return this.requireUgly(modulesName, func);
     }
 
     public async requireUgly<T>(depsArr, func): Promise<T> {
-        var ret = new Deferred();
+        const ret = new Deferred();
 
-        var modules = [];
-        var modulesName = depsArr;
-        var wasMissing = false;
+        const modules = [];
+        const modulesName = depsArr;
+        let wasMissing = false;
         modulesName.forEach(m=> { 
-            var _mod = this.get(m);
+            const _mod = this.get(m);
             if (_mod != null) modules.push(_mod);
             else {
                 wasMissing = true;
@@ -85,7 +85,7 @@ export default class DependencyInjector {
             }
         })
         if (!wasMissing) {
-            var res = func.apply(func, modules);
+            const res = func.apply(func, modules);
             ret.resolve(res);
         }
 
@@ -102,19 +102,19 @@ export default class DependencyInjector {
     }
 
     private _treatReadyPendingFunctions () {
-        var pendingModules = Helpers.getKeys(this._pendingFunctions); //libx.getCustomProperties(this.pendingFunctions)
+        const pendingModules = Helpers.getKeys(this._pendingFunctions); //libx.getCustomProperties(this.pendingFunctions)
         if (Helpers.isEmpty(pendingModules)) return;
 
         pendingModules.forEach(modName=>{
-            var _mod = this.modules[modName];
+            const _mod = this.modules[modName];
             if (_mod == null) return;
-            var pendingArr = [...this._pendingFunctions[modName]];
+            const pendingArr = [...this._pendingFunctions[modName]];
             pendingArr.forEach(pending=>{
-                var modulesName = Helpers.getParamNames(pending.func);
+                const modulesName = Helpers.getParamNames(pending.func);
 
-                var isReady = true;
+                let isReady = true;
                 modulesName.forEach(modName2=>{
-                    var _mod2 = this.modules[modName];
+                    const _mod2 = this.modules[modName];
                     if (_mod2 == null) {
                         isReady = false;
                         return false;
