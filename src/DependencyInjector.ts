1e08bbe7310d94ae4c869321808cf9d71b8efc99
Elya Livshitz <elya.livshitz@gmail.com>
2021-01-25 02:19:50 +0200

    + adding prettier and formating all sources [skip-ci]

--- src/DependencyInjector.ts
+++ src/DependencyInjector.ts
@@ -4,7 +4,7 @@ import Helpers from './Helpers';
 export default class DependencyInjector {
     public modules: Map = {};
     private _pendingFunctions: {};
-    private static resolverType: (name: string)=>Promise<{}>;
+    private static resolverType: (name: string) => Promise<{}>;
     private _resolver: typeof DependencyInjector.resolverType;
 
     constructor(resolver?: typeof DependencyInjector.resolverType) {
@@ -13,11 +13,11 @@ export default class DependencyInjector {
         this._resolver = resolver;
     }
 
-    public register<T>(instance: T, name: string=null): T {
+    public register<T>(instance: T, name: string = null): T {
         if (instance == null) throw new Error('DependencyInjector:register: instance cannot be null');
 
         // backward compatibility where the order of params was opposite
-        if (typeof instance == 'string') { 
+        if (typeof instance == 'string') {
             let tmp: any = name;
             name = instance;
             instance = tmp;
@@ -32,7 +32,7 @@ export default class DependencyInjector {
     public async registerResolve(name, func) {
         const ret = new Deferred();
         const realInstance = await this.require(func);
-        this.register(realInstance, name)
+        this.register(realInstance, name);
         ret.resolve(realInstance);
         return ret;
     }
@@ -42,24 +42,25 @@ export default class DependencyInjector {
 
         let ret = this.modules[name];
 
-        if (ret == null && this._resolver != null) { // try to require a module
+        if (ret == null && this._resolver != null) {
+            // try to require a module
             ret = this._resolver(name);
         }
 
         return ret;
     }
 
-    private resolve<T>(func: Function, isGetArray=false): T[] {
+    private resolve<T>(func: Function, isGetArray = false): T[] {
         const modulesName = Helpers.getParamNames(func);
-        if (modulesName == null) return null
+        if (modulesName == null) return null;
         if (modulesName.length == 1 && !isGetArray) return this.get(modulesName);
 
         let ret = [];
-        modulesName.forEach(m=> ret.push(this.get(m)));
+        modulesName.forEach((m) => ret.push(this.get(m)));
         return ret;
     }
 
-    public inject (func) {
+    public inject(func) {
         const modules = this.resolve(func, true);
         return func.apply(func, modules);
     }
@@ -75,7 +76,7 @@ export default class DependencyInjector {
         const modules = [];
         const modulesName = depsArr;
         let wasMissing = false;
-        modulesName.forEach(m=> { 
+        modulesName.forEach((m) => {
             const _mod = this.get(m);
             if (_mod != null) modules.push(_mod);
             else {
@@ -83,7 +84,7 @@ export default class DependencyInjector {
                 if (this._pendingFunctions[m] == null) this._pendingFunctions[m] = [];
                 this._pendingFunctions[m].push(new DependencyInjector.PendingFunc(func, ret));
             }
-        })
+        });
         if (!wasMissing) {
             const res = func.apply(func, modules);
             ret.resolve(res);
@@ -99,43 +100,41 @@ export default class DependencyInjector {
             this.func = func;
             this.promise = promise;
         }
-    }
+    };
 
-    private _treatReadyPendingFunctions () {
+    private _treatReadyPendingFunctions() {
         const pendingModules = Helpers.getKeys(this._pendingFunctions); //libx.getCustomProperties(this.pendingFunctions)
         if (Helpers.isEmpty(pendingModules)) return;
 
-        pendingModules.forEach(modName=>{
+        pendingModules.forEach((modName) => {
             const _mod = this.modules[modName];
             if (_mod == null) return;
             const pendingArr = [...this._pendingFunctions[modName]];
-            pendingArr.forEach(pending=>{
+            pendingArr.forEach((pending) => {
                 const modulesName = Helpers.getParamNames(pending.func);
 
                 let isReady = true;
-                modulesName.forEach(modName2=>{
+                modulesName.forEach((modName2) => {
                     const _mod2 = this.modules[modName];
                     if (_mod2 == null) {
                         isReady = false;
                         return false;
                     }
-                })
+                });
 
                 if (!isReady) return;
 
                 if (this._pendingFunctions[modName].length == 1) delete this._pendingFunctions[modName];
-                else Helpers.removeItemFromArr(this._pendingFunctions[modName], pending)
+                else Helpers.removeItemFromArr(this._pendingFunctions[modName], pending);
 
-                this.require(pending.func).then(res=>{
+                this.require(pending.func).then((res) => {
                     pending.promise.resolve(res);
                 });
-            })
+            });
         });
     }
-
 }
-    
 
-type Map<T=any>= {
+type Map<T = any> = {
     [Key in keyof T]: T;
-}
+};
--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,43 +1,41 @@
 export default class Helpers {
-    private static STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    private static STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/gm;
     private static ARGUMENT_NAMES = /([^\s,]+)/g;
 
-    public static getParamNames = function(func) { 
-		const fnStr = func.toString().replace(Helpers.STRIP_COMMENTS, '');
-		if (fnStr.match(/^\s*class\s+/) != null) return null;
-		const m = fnStr.match(/^\(?(?:async\s?)?(?:function\s?)?\(?([\w\d\,\s\$\_]+)\)?/);
-		if (m == null || m.length < 1) return null;
-		const params = m[1].replace(/\s/g, '');
-		let result = params.split(',');
-		// let result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-		if(result === null)
-			result = [];
-		return result;
-    }
-    
-    public static getKeys = (obj)=>{
-        return Object.keys(obj);
-    }
+    public static getParamNames = function (func) {
+        const fnStr = func.toString().replace(Helpers.STRIP_COMMENTS, '');
+        if (fnStr.match(/^\s*class\s+/) != null) return null;
+        const m = fnStr.match(/^\(?(?:async\s?)?(?:function\s?)?\(?([\w\d\,\s\$\_]+)\)?/);
+        if (m == null || m.length < 1) return null;
+        const params = m[1].replace(/\s/g, '');
+        let result = params.split(',');
+        // let result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+        if (result === null) result = [];
+        return result;
+    };
 
-    public static isEmpty = (obj)=>{
+    public static getKeys = (obj) => {
+        return Object.keys(obj);
+    };
+
+    public static isEmpty = (obj) => {
         if (obj == null) return true;
-        for(const prop in obj) {
-            if(obj.hasOwnProperty(prop))
-                return false;
+        for (const prop in obj) {
+            if (obj.hasOwnProperty(prop)) return false;
         }
 
         return JSON.stringify(obj) === JSON.stringify({});
-    }
+    };
 
-    public static removeItemFromArr = (arr, item) =>{
+    public static removeItemFromArr = (arr, item) => {
         const index = arr.indexOf(item);
-		if (index > -1) {
-			arr.splice(index, 1);
-		}
-		return arr;
-    }
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        return arr;
+    };
 
-    public static tryGetInstanceTypeName = function<T>(instance?: T): string {
+    public static tryGetInstanceTypeName = function <T>(instance?: T): string {
         if (instance == null) return null;
         if (typeof instance == 'function') {
             if (instance.name == '') throw new Error('Helpers:tryGetInstanceName: Cannot imply instance name from anonymous functions');
@@ -46,5 +44,5 @@ export default class Helpers {
         if (instance.constructor?.name && instance.constructor?.name != 'Function') return instance.constructor?.name;
 
         return typeof instance;
-    }
+    };
 }
